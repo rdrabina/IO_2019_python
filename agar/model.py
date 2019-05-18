0efc3e577efa0d1d25eb1af02e82002597cf6946
Jean9797 <rodzonjan97@gmail.com>
2019-05-18 12:24:53 +0200

    model extended

--- agar/model.py
+++ agar/model.py
@@ -1,9 +1,22 @@
+from collision import Vector
+
+
 class GameState(object):
     def __init__(self):
         players = []
-        objects = []
+        plankton = []
 
 
 class MapObject:
     def __init__(self, x=0, y=0):
         coordinates = (x, y)
+
+
+class Player(MapObject):
+    def __init__(self, login, department, image=None):
+        super(Player, self).__init__(5, 5)
+        self.login = login
+        self.department = department
+        self.image = image
+        self.velocity = 0
+        self.direction = Vector(0, 0)
