98a7299a286655c54b711ecf8f3b90d7104fd2bc
kmakiela <makiela11@gmail.com>
2019-05-18 10:29:36 +0200

    json parsing works

--- agar/server.py
+++ agar/server.py
@@ -2,6 +2,13 @@ import socketserver
 import json
 
 
+def parse_bytes_to_json(data):
+    data_str = data.decode('utf8').replace("'", '"')
+    data_json = json.loads(data_str)
+    out = json.dumps(data_json, indent=4, sort_keys=True)
+    return out
+
+
 class MyTCPHandler(socketserver.BaseRequestHandler):
 
     def __init__(self):
@@ -11,10 +18,9 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
         print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        print(json.dumps(self.data))
+        out = parse_bytes_to_json(self.data)
         # just send back the same data, but upper-cased
-        self.request.sendall(json.dumps(self.data))
+        self.request.sendall(out)
 
 
 if __name__ == "__main__":
