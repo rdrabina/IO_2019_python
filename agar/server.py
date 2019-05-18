c18e923d997908362f25754c9d987c966839dcc7
Tomasz Kasprzycki <kasprzyckit@gmail.com>
2019-05-18 09:55:07 +0200

    decoding

--- agar/server.py
+++ agar/server.py
@@ -1,15 +1,27 @@
-import threading
 import socketserver
 
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    """
+    The request handler class for our server.
 
-class TCPHandler(socketserver.BaseRequestHandler):
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
     def handle(self):
-        self.request.sendall("hello")
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print("{} wrote:".format(self.client_address[0]))
+        print(self.data.decode('utf-8'))
+        # just send back the same data, but upper-cased
+        self.request.sendall(self.data.upper().encode('utf-8'))
 
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
 
-def run():
-    print("server started")
-    HOST, PORT = "localhost", "9999"
-    server = socketserver.TCPServer((HOST, PORT), TCPHandler)
-    server.serve_forever()
-
+    # Create the server, binding to localhost on port 9999
+    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
+        # Activate the server; this will keep running until you
+        # interrupt the program with Ctrl-C
+        server.serve_forever()
